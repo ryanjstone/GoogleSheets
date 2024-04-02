20179515a8399d6ef5eea898a67f83631103d0ee
Ryan Stone <ryanjstone@gmail.com>
2024-04-02 13:52:40 +0100

    Integrates Streamlit with Google Sheets

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
-st.write("Hello world")
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read(worksheet="Weighings")
+
+st.dataframe(df)

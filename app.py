226577ff100a151f5a30d7f8548561bca2749fe3
Ryan Stone <ryanjstone@gmail.com>
2024-04-02 14:09:36 +0100

    Add comments

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+# Import Streamlit
 import streamlit as st
+
+# Import Streamlit GSheets module
 from streamlit_gsheets import GSheetsConnection
 
+# Connect to Google Sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
 
+# Read Weighings worksheet
 df = conn.read(worksheet="Weighings")
 
+# Print dataframe
 st.dataframe(df)
